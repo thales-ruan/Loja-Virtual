d0ec1ed1521f0b67e1cd7bcd33219f762a7e039e
Thales Ruan <thalesruandamassacla@gmail.com>
2023-02-13 21:58:32 -0300

    ajustes

---
+++ app/src/main/java/com/example/lojavirtual/formCadastro/FormCadastroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lojavirtual.formCadastro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lojavirtual.R
+
+class FormCadastroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_cadastro)
+    }
+}
--- app/src/main/java/com/example/lojavirtual/formLogin/FormLoginActivity.kt
+++ app/src/main/java/com/example/lojavirtual/formLogin/FormLoginActivity.kt
@@ -1,11 +1,12 @@
-package com.example.lojavirtual
+package com.example.lojavirtual.formLogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lojavirtual.R
 
-class MainActivity : AppCompatActivity() {
+class FormLoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_form_login)
     }
 }
