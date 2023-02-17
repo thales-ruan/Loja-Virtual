79165e917d4d6cd207c73853ba18541a8c084269
Thales Ruan <thalesruandamassacla@gmail.com>
2023-02-16 21:58:35 -0300

    tela login

--- app/src/main/java/com/example/lojavirtual/formLogin/FormLoginActivity.kt
+++ app/src/main/java/com/example/lojavirtual/formLogin/FormLoginActivity.kt
@@ -1,12 +1,26 @@
 package com.example.lojavirtual.formLogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.lojavirtual.R
+import com.example.lojavirtual.databinding.ActivityFormCadastroBinding
+import com.example.lojavirtual.databinding.ActivityFormLoginBinding
 
 class FormLoginActivity : AppCompatActivity() {
+    lateinit var binding : ActivityFormLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_login)
+        binding = ActivityFormLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.cliqueAqui.setOnClickListener {
+            startActivity(Intent(applicationContext,ActivityFormCadastroBinding::class.java))
+        }
+
     }
+
+
 }
