ee4a7aaa29f5acada96c0785ef58c42c4d759cb0
Thales Ruan <thalesruandamassacla@gmail.com>
2023-02-20 20:19:54 -0300

    ajustes

--- app/src/main/java/com/example/lojavirtual/formCadastro/FormCadastroActivity.kt
+++ app/src/main/java/com/example/lojavirtual/formCadastro/FormCadastroActivity.kt
@@ -2,11 +2,21 @@ package com.example.lojavirtual.formCadastro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.lojavirtual.R
+import com.example.lojavirtual.databinding.ActivityFormCadastroBinding
+import com.example.lojavirtual.databinding.ActivityFormLoginBinding
 
 class FormCadastroActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityFormCadastroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityFormCadastroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         setContentView(R.layout.activity_form_cadastro)
     }
 }
